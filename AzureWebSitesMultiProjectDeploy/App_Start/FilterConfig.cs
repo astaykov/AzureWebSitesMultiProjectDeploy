b8e937be3756a0a9f6800b5c880aedffe2eadd3c
Anton Staykov <astaykov@gmail.com>
2014-07-25 10:38:23 +0200

    initial commit

---
+++ AzureWebSitesMultiProjectDeploy/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AzureWebSitesMultiProjectDeploy
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ AzureWebSitesMultiProjectDeploy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureWebSitesMultiProjectDeploy.Startup))]
+namespace AzureWebSitesMultiProjectDeploy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
